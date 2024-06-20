6f7c31f0088f9661a747982e3bb9097e2146e27c
Humaira Islam <ihumaira.webfront@gmail.com>
2024-06-20 20:28:32 +0600

    All products viewed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,35 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Home = () => {
 
-    axios.get('https://api.jsonbin.io/v3/b/6673d8f8ad19ca34f87bc010')
-    .then((res)=> console.log(res))
+    const[productData, setProductData]=useState([])
+
+
+    useEffect(()=>{
+        axios.get('https://api.jsonbin.io/v3/b/6673dd4facd3cb34a85a3f89')
+        .then((res)=> setProductData(res.data.record))
+        .catch((err)=> console.log(err))
+    },[])
+    
+    console.log(productData)
+
   return (
     <>
       <section className='p-10'>
         <div className='container'>
             <div className='mainRow flex gap-5 flex-wrap justify-center'>
-                <div className='singleCard w-[250px] h-[590px] bg-[#FFFED3] p-5 rounded-lg'>
+                {
+                    productData.map((item,i)=>(
+                        <div key={i} className='singleCard w-[250px] h-[590px] bg-[#FFFED3] p-5 rounded-lg'>
                     <div className='productImage w-full h-[130px] overflow-hidden'>
-                        <img className='w-full h-full' src="" alt="" />
+                        <img className='w-full h-full' src={item.image_link} alt="" />
                     </div>
                     <div className='product_text'>
-                        <h2 className='text-[18px] font-semibold text-black mt-5 mb-5'>Product name</h2>
+                        <h2 className='text-[18px] font-semibold text-black mt-5 mb-5'>{item.product_title}</h2>
                         <ul className='list-desc p-5'>
-                            <li>brand</li>
-                            <li>processor</li>
-                            <li>RAM</li>
-                            <li>Charging capacity</li>
-                            <li>Display</li>
-                            <li>Display resolution</li>
+                            <li>{item.brand}</li>
+                            <li>{item.processor}</li>
+                            <li>{item.ram}</li>
+                            <li>{item.charging_capacity}</li>
+                            <li>{item.display_type}</li>
+                            <li>{item.display_resolution}</li>
                         </ul>
                         <hr />
-                        <h4 className='mt-5 mb-2 text-[20px] font-bold text-[#FF7F3E] text-center'>Price: </h4>
+                        <h4 className='mt-5 mb-2 text-[20px] font-bold text-[#FF7F3E] text-center'>Price: {item.price} </h4>
                         <button className='w-full h-[40px] flex justify-center items-center bg-[#BBE9FF] rounded-lg font-bold'>Buy Now</button>
                         <button className='w-full h-[40px] flex justify-center items-center bg-[#B1AFFF] rounded-lg font-bold mt-2'>View details</button>
                     </div>                    
-                </div>      
+                </div>  
+
+                    ))
+                }  
                
             </div>
 
